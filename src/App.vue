de44d376314f3185a4727c2fc316f9ed85e69549
Akanimo Sam Okure <akan.okure@gmail.com>
2023-01-02 09:24:33 +0000

    add and impliments components, Maxins and functinal components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,74 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <MaterialCardBox
+      header="Material Card Header"
+      sub-header="Card Sub Header"
+      show-media
+      show-actions
+      img-src="https://picsum.photos/300/200"
+    >
+      <p>
+        <StarRating ref="starRating" />
+      </p>
+      <p>
+        The path of the righteous man is beset on all sides by the iniquities of
+        the selfish and the tyranny of evil men.
+      </p>
+      <template v-slot:action>
+        <MaterialButton
+          background-color="#027be3"
+          text-color="#fff"
+          @click="resetVote"
+        >
+          Reset
+        </MaterialButton>
+        <MaterialButton
+          background-color="#26a69a"
+          text-color="#fff"
+          is-flat
+          @click="forceVote"
+        >
+          Rate 5 Stars
+        </MaterialButton>
+      </template>
+    </MaterialCardBox>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { defineAsyncComponent } from "vue";
+
+const StarRating = defineAsyncComponent(() =>
+  import("./components/StarRating.vue")
+);
+const MaterialButton = defineAsyncComponent(() =>
+  import("./components/MaterialButton.vue")
+);
+const MaterialCardBox = defineAsyncComponent(() =>
+  import("./components/MaterialCardBox.vue")
+);
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    StarRating,
+    MaterialButton,
+    MaterialCardBox,
+  },
+  methods: {
+    resetVote() {
+      this.$refs.starRating.rank = 0;
+      this.$refs.starRating.voted = false;
+    },
+    forceVote() {
+      this.$refs.starRating.rank = 5;
+      this.$refs.starRating.voted = true;
+    },
+  },
+};
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  font-size: 14px;
 }
 </style>
